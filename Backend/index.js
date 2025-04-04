e24bca227094516b5536d8c860772681f5e311dc
Harish <harishkannaaravindan@gmail.com>
2025-04-04 23:08:14 +0530

    book routes

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 import express from "express";
 import connectdb from "./db.js";
 import bookRouter from "./routes/bookroutes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-import list from "../../public/list.json";
 import Cards from "./Cards";
 function Freebook() {
-  const filterData = list.filter((data) => data.category === "Free");
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/book");
+        console.log(res.data);
+        const filterData = res.data.filter((data) => data.category === "Free");
+        setBook(filterData);
+      } catch (err) {
+        console.log("Error", err);
+      }
+    };
+    getBook();
+  }, []);
 
   var settings = {
     dots: true,
@@ -57,7 +69,7 @@ function Freebook() {
 
         <div>
           <Slider {...settings}>
-            {filterData.map((item) => (
+            {book.map((item) => (
               <Cards item={item} key={item.id} />
             ))}
           </Slider>
